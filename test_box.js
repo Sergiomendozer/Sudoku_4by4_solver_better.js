78f2a708dd89ad4c4dfc98420200481a379d170c
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-11 02:36:35 -0800

    solver does not solve correctly due to the fact it does not take into account square

---
+++ test_box.js
@@ -0,0 +1,19 @@
+var sudoku9_1 = [
+  [3, 0, 0, 2, 0, 0, 0, 9, 0],
+  [4, 5, 0, 6, 0, 9, 0, 1, 8],
+  [7, 0, 6, 0, 0, 3, 4, 0, 0],
+  [0, 0, 0, 0, 4, 1, 0, 0, 9],
+  [5, 0, 7, 3, 0, 8, 2, 0, 0],
+  [9, 8, 0, 7, 0, 0, 5, 0, 4],
+  [0, 0, 0, 4, 0, 5, 0, 2, 0],
+  [1, 3, 0, 0, 6, 0, 7, 0, 5],
+  [0, 2, 0, 0, 8, 0, 0, 0, 0],
+];
+var r = 0;
+var c = 0;
+
+var r_in_which_box = null;
+var c_in_which_box = null;
+console.log(`r_in_which_box  ${r_in_which_box}`);
+console.log(`c_in_which_box  ${c_in_which_box}`);
+console.log(sudoku9_1[0]);
