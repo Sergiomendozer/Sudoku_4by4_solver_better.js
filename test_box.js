e0152c3a48c31cde85b262d48a282708316b9694
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-11 04:01:03 -0800

    testing

--- test_box.js
+++ test_box.js
@@ -9,6 +9,16 @@ var c = 3;
 
 var r_in_which_box = Math.floor(r / 2);
 var c_in_which_box = Math.floor(c / 2);
+var r_end_of_box = r_in_which_box + 2;
+var c_end_of_box = c_in_which_box + 2;
 console.log(`r_in_which_box  ${r_in_which_box}`);
 console.log(`c_in_which_box  ${c_in_which_box}`);
-console.log(sudoku_1[0]);
+console.log(`r_end ${r_end_of_box}`);
+console.log(`c_end ${c_end_of_box}`);
+// for (var r_box = r_in_which_box; r_in_which_box < r_end_of_box; r_box++) {
+//   for (var c_box = c_in_which_box; c_in_which_box < c_end_of_box; c_box++) {
+//     // console.log(`r_box ${r_box}`);
+//     // console.log(`c_box ${c_box}`);
+//     // console.log(sudoku_1[r_box][c_box]);
+//   }
+// }
