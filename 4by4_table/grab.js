cc5a36c6cb19f682208e5649022f7b23f5eda9e3
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-13 16:12:44 -0700

    grabs elements id

--- 4by4_table/grab.js
+++ 4by4_table/grab.js
@@ -2,12 +2,13 @@
 // var tt = document.getElementsByTagName("input")[0].value;
 // console.log(tt);
 var c0r0 = null;
-function get_value_c0r0() {
-  c0r0 = document.getElementById("c0r0").value;
-  console.log("c0r0:" + c0r0);
-  if (c0r0.length <= 0) {
-    c0r0 = 0;
+var id = null;
+function get_value(event, id) {
+  id = document.getElementById(id).value;
+  console.log("c0r0:" + id);
+  if (id.length <= 0) {
+    var c0r0 = 0;
     console.log("set to zero");
-    console.log("c0r0:" + c0r0);
+    console.log("c0r0:" + id);
   }
 }
