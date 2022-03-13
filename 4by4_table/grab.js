1a11b9f0b834614168e28d0be67f8360788a98e4
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-13 15:57:58 -0700

    grabs user input and puts into js

--- 4by4_table/grab.js
+++ 4by4_table/grab.js
@@ -1,3 +1,11 @@
-// var c0r0 = document.getElementsById("c0r0").value;
-var tt = document.getElementsByTagName("input")[0].value;
-console.log(tt);
+// // var c0r0 = document.getElementsById("c0r0").value;
+// var tt = document.getElementsByTagName("input")[0].value;
+// console.log(tt);
+
+function get_value_c0r0() {
+  var c0r0 = document.getElementById("c0r0").value;
+  console.log("c0r0:" + c0r0);
+  if (c0r0.length <= 0) {
+    console.log("zero");
+  }
+}
