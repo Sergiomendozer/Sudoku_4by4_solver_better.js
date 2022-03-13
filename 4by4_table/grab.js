a0acfc9b397abca1811e306948cd88da728a0970
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-13 16:00:59 -0700

    sets to zero, var is above

--- 4by4_table/grab.js
+++ 4by4_table/grab.js
@@ -1,11 +1,13 @@
 // // var c0r0 = document.getElementsById("c0r0").value;
 // var tt = document.getElementsByTagName("input")[0].value;
 // console.log(tt);
-
+var c0r0 = null;
 function get_value_c0r0() {
-  var c0r0 = document.getElementById("c0r0").value;
+  c0r0 = document.getElementById("c0r0").value;
   console.log("c0r0:" + c0r0);
   if (c0r0.length <= 0) {
-    console.log("zero");
+    c0r0 = 0;
+    console.log("set to zero");
+    console.log("c0r0:" + c0r0);
   }
 }
