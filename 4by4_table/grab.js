55c960ab652dc6c7c3f634729ed1d6243828441a
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-14 04:52:16 -0700

    examples added and connect to buttons

--- 4by4_table/grab.js
+++ 4by4_table/grab.js
@@ -10,6 +10,36 @@ var sudoku_table = [
   [0, 0, 0, 0],
   [0, 0, 0, 0],
 ];
+var sudoku_hardest = [
+  [0, 0, 4, 0],
+  [1, 0, 0, 0],
+  [0, 2, 0, 0],
+  [0, 0, 0, 3],
+];
+var sudoku_5 = [
+  [0, 3, 0, 0],
+  [1, 0, 0, 3],
+  [0, 0, 0, 1],
+  [2, 0, 0, 0],
+];
+var sudoku_6 = [
+  [3, 0, 0, 0],
+  [0, 4, 3, 0],
+  [1, 0, 0, 2],
+  [0, 2, 1, 0],
+];
+var sudoku_7 = [
+  [0, 4, 0, 2],
+  [1, 0, 0, 0],
+  [0, 0, 3, 0],
+  [4, 0, 0, 1],
+];
+var sudoku_20 = [
+  [3, 0, 0, 0],
+  [0, 4, 1, 0],
+  [0, 2, 0, 0],
+  [1, 0, 4, 0],
+];
 
 var c0r0 = null;
 var value = null;
@@ -36,6 +66,133 @@ function clear_all() {
   document.getElementById("c3r2").value = "";
   document.getElementById("c3r3").value = "";
   sudoku_table = sudoku_reset;
+  console.table(sudoku_table);
+}
+
+function example_1() {
+  console.log("example_1"); // !delete for testing
+  console.log(sudoku_table); //// !delete for testing
+  document.getElementById("c0r0").value = "";
+  document.getElementById("c0r1").value = "";
+  document.getElementById("c0r2").value = "4";
+  document.getElementById("c0r3").value = "";
+
+  document.getElementById("c1r0").value = "1";
+  document.getElementById("c1r1").value = "";
+  document.getElementById("c1r2").value = "";
+  document.getElementById("c1r3").value = "";
+
+  document.getElementById("c2r0").value = "";
+  document.getElementById("c2r1").value = "2";
+  document.getElementById("c2r2").value = "";
+  document.getElementById("c2r3").value = "";
+
+  document.getElementById("c3r0").value = "";
+  document.getElementById("c3r1").value = "";
+  document.getElementById("c3r2").value = "";
+  document.getElementById("c3r3").value = "3";
+  sudoku_table = sudoku_hardest;
+  console.table(sudoku_table);
+}
+function example_2() {
+  console.log("example_2"); // !delete for testing
+  console.log(sudoku_table); // !delete for testing
+  document.getElementById("c0r0").value = "";
+  document.getElementById("c0r1").value = "3";
+  document.getElementById("c0r2").value = "";
+  document.getElementById("c0r3").value = "";
+
+  document.getElementById("c1r0").value = "1";
+  document.getElementById("c1r1").value = "";
+  document.getElementById("c1r2").value = "";
+  document.getElementById("c1r3").value = "3";
+
+  document.getElementById("c2r0").value = "";
+  document.getElementById("c2r1").value = "";
+  document.getElementById("c2r2").value = "";
+  document.getElementById("c2r3").value = "1";
+
+  document.getElementById("c3r0").value = "2";
+  document.getElementById("c3r1").value = "";
+  document.getElementById("c3r2").value = "";
+  document.getElementById("c3r3").value = "";
+  sudoku_table = sudoku_5;
+  console.table(sudoku_table);
+}
+function example_3() {
+  console.log("example_3"); // !delete for testing
+  console.log(sudoku_table); // !delete for testing
+  document.getElementById("c0r0").value = "3";
+  document.getElementById("c0r1").value = "";
+  document.getElementById("c0r2").value = "";
+  document.getElementById("c0r3").value = "";
+
+  document.getElementById("c1r0").value = "";
+  document.getElementById("c1r1").value = "4";
+  document.getElementById("c1r2").value = "3";
+  document.getElementById("c1r3").value = "";
+
+  document.getElementById("c2r0").value = "1";
+  document.getElementById("c2r1").value = "";
+  document.getElementById("c2r2").value = "";
+  document.getElementById("c2r3").value = "2";
+
+  document.getElementById("c3r0").value = "";
+  document.getElementById("c3r1").value = "2";
+  document.getElementById("c3r2").value = "1";
+  document.getElementById("c3r3").value = "";
+  sudoku_table = sudoku_6;
+  console.table(sudoku_table);
+}
+function example_4() {
+  console.log("example_4"); // !delete for testing
+  console.log(sudoku_table); // !delete for testing
+  document.getElementById("c0r0").value = "";
+  document.getElementById("c0r1").value = "4";
+  document.getElementById("c0r2").value = "";
+  document.getElementById("c0r3").value = "2";
+
+  document.getElementById("c1r0").value = "";
+  document.getElementById("c1r1").value = "1";
+  document.getElementById("c1r2").value = "";
+  document.getElementById("c1r3").value = "";
+
+  document.getElementById("c2r0").value = "";
+  document.getElementById("c2r1").value = "";
+  document.getElementById("c2r2").value = "3";
+  document.getElementById("c2r3").value = "";
+
+  document.getElementById("c3r0").value = "4";
+  document.getElementById("c3r1").value = "";
+  document.getElementById("c3r2").value = "";
+  document.getElementById("c3r3").value = "1";
+
+  sudoku_table = sudoku_7;
+  console.table(sudoku_table);
+}
+function example_5() {
+  console.log("example_5"); // !delete for testing
+  console.log(sudoku_table); // !delete for testing
+  document.getElementById("c0r0").value = "3";
+  document.getElementById("c0r1").value = "";
+  document.getElementById("c0r2").value = "";
+  document.getElementById("c0r3").value = "";
+
+  document.getElementById("c1r0").value = "";
+  document.getElementById("c1r1").value = "4";
+  document.getElementById("c1r2").value = "1";
+  document.getElementById("c1r3").value = "";
+
+  document.getElementById("c2r0").value = "";
+  document.getElementById("c2r1").value = "2";
+  document.getElementById("c2r2").value = "";
+  document.getElementById("c2r3").value = "";
+
+  document.getElementById("c3r0").value = "1";
+  document.getElementById("c3r1").value = "";
+  document.getElementById("c3r2").value = "4";
+  document.getElementById("c3r3").value = "";
+  sudoku_table = sudoku_20;
   console.table(sudoku_table);
 }
 
@@ -88,4 +245,10 @@ function get_value(event, id) {
     console.log(position + value);
   }
   return inputted_sudoku_table(position, value);
+}
+
+function start() {
+  var table = sudoku_table;
+  console.table(table); //! for testing
+  console.log("start"); //! for testing
 }
