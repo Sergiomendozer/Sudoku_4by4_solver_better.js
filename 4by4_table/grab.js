02d383fa507727aea35ec2103f3bccf87e9464aa
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-14 00:55:03 -0700

    backend mutates all position of table

--- 4by4_table/grab.js
+++ 4by4_table/grab.js
@@ -1,6 +1,3 @@
-// // var c0r0 = document.getElementsById("c0r0").value;
-// var tt = document.getElementsByTagName("input")[0].value;
-// console.log(tt);
 var sudoku_0 = [
   [0, 0, 0, 0],
   [0, 0, 0, 0],
@@ -20,13 +17,43 @@ function inputted_sudoku_table(position, value) {
   console.log(position, value);
   if (position == "c0r0") {
     sudoku_table[0][0] = parseInt(value);
+  } else if (position == "c0r1") {
+    sudoku_table[0][1] = parseInt(value);
+  } else if (position == "c0r2") {
+    sudoku_table[0][2] = parseInt(value);
+  } else if (position == "c0r3") {
+    sudoku_table[0][3] = parseInt(value);
+  } else if (position == "c1r0") {
+    sudoku_table[1][0] = parseInt(value);
+  } else if (position == "c1r1") {
+    sudoku_table[1][1] = parseInt(value);
+  } else if (position == "c1r2") {
+    sudoku_table[1][2] = parseInt(value);
+  } else if (position == "c1r3") {
+    sudoku_table[1][3] = parseInt(value);
+  } else if (position == "c2r0") {
+    sudoku_table[2][0] = parseInt(value);
+  } else if (position == "c2r1") {
+    sudoku_table[2][1] = parseInt(value);
+  } else if (position == "c2r2") {
+    sudoku_table[2][2] = parseInt(value);
+  } else if (position == "c2r3") {
+    sudoku_table[2][3] = parseInt(value);
+  } else if (position == "c3r0") {
+    sudoku_table[3][0] = parseInt(value);
+  } else if (position == "c3r1") {
+    sudoku_table[3][1] = parseInt(value);
+  } else if (position == "c3r2") {
+    sudoku_table[3][2] = parseInt(value);
+  } else if (position == "c3r3") {
+    sudoku_table[3][3] = parseInt(value);
   }
   console.table(sudoku_table);
 }
 function get_value(event, id) {
   var position = id;
   value = document.getElementById(id).value;
-  //   console.log("c0r0:" + value); //! delete
+  //   console.log("c0r0:" + value); //! delete, testing
   if (value.length <= 0) {
     var value = 0;
     console.log("set to zero");
@@ -34,6 +61,3 @@ function get_value(event, id) {
   }
   return inputted_sudoku_table(position, value);
 }
-// if (position == "c0r0") {
-//   console.log("pos c0r0");
-// }
