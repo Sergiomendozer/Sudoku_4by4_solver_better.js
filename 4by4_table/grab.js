10557272a96b008efede684e14f03ec6927be95b
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-14 00:38:58 -0700

    new function

--- 4by4_table/grab.js
+++ 4by4_table/grab.js
@@ -2,13 +2,21 @@
 // var tt = document.getElementsByTagName("input")[0].value;
 // console.log(tt);
 var c0r0 = null;
-var id = null;
-function get_value(event, id) {
-  id = document.getElementById(id).value;
-  console.log("c0r0:" + id);
-  if (id.length <= 0) {
-    var c0r0 = 0;
-    console.log("set to zero");
-    console.log("c0r0:" + id);
-  }
+var value = null;
+function inputted_sudoku_table(position, value) {
+  console.log(position, value);
 }
+function get_value(event, id) {
+  var position = id;
+  value = document.getElementById(id).value;
+  //   console.log("c0r0:" + value); //! delete
+  if (value.length <= 0) {
+    var value = 0;
+    console.log("set to zero");
+    console.log("c0r0:" + value);
+  }
+  return inputted_sudoku_table(position, value);
+}
+// if (position == "c0r0") {
+//   console.log("pos c0r0");
+// }
