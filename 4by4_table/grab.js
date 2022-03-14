3034e96f4e857653bfdb18da679db408d7323c5c
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-14 00:48:38 -0700

    input position is grabed and changes sudoku table

--- 4by4_table/grab.js
+++ 4by4_table/grab.js
@@ -1,10 +1,27 @@
 // // var c0r0 = document.getElementsById("c0r0").value;
 // var tt = document.getElementsByTagName("input")[0].value;
 // console.log(tt);
+var sudoku_0 = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+];
+var sudoku_table = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+];
+
 var c0r0 = null;
 var value = null;
 function inputted_sudoku_table(position, value) {
   console.log(position, value);
+  if (position == "c0r0") {
+    sudoku_table[0][0] = parseInt(value);
+  }
+  console.table(sudoku_table);
 }
 function get_value(event, id) {
   var position = id;
@@ -13,7 +30,7 @@ function get_value(event, id) {
   if (value.length <= 0) {
     var value = 0;
     console.log("set to zero");
-    console.log("c0r0:" + value);
+    console.log(position + value);
   }
   return inputted_sudoku_table(position, value);
 }
