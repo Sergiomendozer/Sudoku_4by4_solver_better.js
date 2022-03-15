92cc8645319ff3ba7b2917ac3812d4756c059fa7
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-15 01:02:05 -0700

    comments used for testing deleted

--- 4by4_table/grab.js
+++ 4by4_table/grab.js
@@ -238,7 +238,6 @@ function inputted_sudoku_table(position, value) {
 function get_value(event, id) {
   var position = id;
   value = document.getElementById(id).value;
-  //   console.log("c0r0:" + value); //! delete, testing
   if (value.length <= 0) {
     var value = 0;
     console.log("set to zero");
@@ -248,13 +247,6 @@ function get_value(event, id) {
 }
 //////////////////////////////////////////////////////////////////////////
 function find_empty_space(table) {
-  console.log("function find_empty_space()"); //!used for testing
-  console.table(table); //!for testing it displays table
-  var s = table[0].indexOf(0); //!used for testing
-  var d = table[1].indexOf(0); //!used for testing
-  var f = table[2].indexOf(0); //!used for testing
-  var g = table[3].indexOf(0); //!used for testing
-  console.log(s, d, f, g); //!used for testing
   // case where the sudoku table is solved and there are not more empty spaces
   if (
     table[0].indexOf(0) == -1 &&
@@ -262,16 +254,7 @@ function find_empty_space(table) {
     table[2].indexOf(0) == -1 &&
     table[3].indexOf(0) == -1
   ) {
-    //below updates string in html //! delete
-    // var first_col = table[0];
-    // var second_col = table[1];
-    // var third_col = table[2];
-    // var forth_col = table[3];
-    // document.getElementById("table_1").innerHTML = first_col;
-    // document.getElementById("table_2").innerHTML = second_col;
-    // document.getElementById("table_3").innerHTML = third_col;
-    // document.getElementById("table_4").innerHTML = forth_col;
-    //!delete
+    // changes HTML elements
     document.getElementById("c0r0").value = table[0][0];
     document.getElementById("c0r1").value = table[0][1];
     document.getElementById("c0r2").value = table[0][2];
@@ -300,8 +283,6 @@ function find_empty_space(table) {
   else {
     for (var r = 0; r < 5; r++) {
       for (var c = 0; c < 5; c++) {
-        // console.log(`row: ${r}`); //!for testing
-        // console.log(`column: ${c}`); //!for testing
         // if object in array is equal to  0 then it means the space is empty
         if (table[r][c] == 0) {
           return [r, c];
@@ -313,11 +294,8 @@ function find_empty_space(table) {
 // a number n is picked from 1 to 4 and in the empty position the function checks if n can go in the empty space
 //n can only be put in the empty space if it follows rules of sudoku
 function check_if_number_can_go_in_position(table, n, r, c) {
-  console.log("function check_if_number_can_go_in_position()"); //!for testing
-  console.log(`row ${table[r]}`); //!for testing
   // var below makes a array of tables chosen column that is need to search if n can be placed in empty position
   var column_c = table.map((d) => d[c]);
-  console.log(`col ${column_c}`); //!for testing
   //case if empty position's row has number n already then the it need to re-evaluate the previous situation
   if (table[r].indexOf(n) != -1) {
     console.log("re-evaluate b/c r");
@@ -340,16 +318,12 @@ function sudoku_solver(table) {
   // if values is false then the sudoku table is solved
   // if values is true then the sudoku table is not solved
   if (values === true) return true;
-  console.log(values); //! for testing
   var r = values[0];
   var c = values[1];
-  console.log("in one"); //! for in testing
   // for loop, is so 1 to 4 sudoku numbers get run into empty position
   for (var n = 1; n < 5; n++) {
-    console.log(`n = ${n}`); //! for in testing
     if (check_if_number_can_go_in_position(table, n, r, c) == true) {
       table[r][c] = n;
-      console.table(table); //! for in testing
       if (sudoku_solver(table) === true) return true;
     }
     // if function (check_if_number_can_go_in_position(table, n, r, c) == false)
@@ -361,7 +335,5 @@ function sudoku_solver(table) {
 
 function start() {
   var table = sudoku_table;
-  console.log("start"); //! for testing
-  console.table(table); //! for testing
   sudoku_solver(table);
 }
