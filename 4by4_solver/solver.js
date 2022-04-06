a0e86d494515a085cdbbccc859bab6a7c56bfd66
sergiomendozer <sergiomendozansx@gmail.com>
2022-04-06 12:21:22 -0700

    var to let in 4by4 solver.js file

--- 4by4_solver/solver.js
+++ 4by4_solver/solver.js
@@ -4,43 +4,43 @@ const sudoku_reset = [
   [0, 0, 0, 0],
   [0, 0, 0, 0],
 ];
-var sudoku_table = [
+let sudoku_table = [
   [0, 0, 0, 0],
   [0, 0, 0, 0],
   [0, 0, 0, 0],
   [0, 0, 0, 0],
 ];
-var sudoku_hardest = [
+let sudoku_hardest = [
   [0, 0, 4, 0],
   [1, 0, 0, 0],
   [0, 2, 0, 0],
   [0, 0, 0, 3],
 ];
-var sudoku_5 = [
+let sudoku_5 = [
   [0, 3, 0, 0],
   [1, 0, 0, 3],
   [0, 0, 0, 1],
   [2, 0, 0, 0],
 ];
-var sudoku_6 = [
+let sudoku_6 = [
   [3, 0, 0, 0],
   [0, 4, 3, 0],
   [1, 0, 0, 2],
   [0, 2, 1, 0],
 ];
-var sudoku_7 = [
+let sudoku_7 = [
   [0, 4, 0, 2],
   [1, 0, 0, 0],
   [0, 0, 3, 0],
   [4, 0, 0, 1],
 ];
-var sudoku_20 = [
+let sudoku_20 = [
   [3, 0, 0, 0],
   [0, 4, 1, 0],
   [0, 2, 0, 0],
   [1, 0, 4, 0],
 ];
-var value = null;
+let value = null;
 // function resets table and clears user inputs
 function clear_all() {
   document.getElementById("c0r0").value = "";
@@ -222,10 +222,10 @@ function inputted_sudoku_table(position, value) {
 }
 // function grabs value form html that is inputted by user
 function get_value(event, id) {
-  var position = id;
+  let position = id;
   value = document.getElementById(id).value;
   if (value.length <= 0) {
-    var value = 0;
+    let value = 0;
   }
   return inputted_sudoku_table(position, value);
 }
@@ -263,10 +263,10 @@ function find_empty_space(table) {
     return true;
   }
   //for loops go through 1-4
-  // var meanings r = row, c = column
+  // let meanings r = row, c = column
   else {
-    for (var r = 0; r < 5; r++) {
-      for (var c = 0; c < 5; c++) {
+    for (let r = 0; r < 5; r++) {
+      for (let c = 0; c < 5; c++) {
         // if object in array is equal to  0 then it means the space is empty
         if (table[r][c] == 0) {
           return [r, c];
@@ -278,8 +278,8 @@ function find_empty_space(table) {
 // a number n is picked from 1 to 4 and in the empty position the function checks if n can go in the empty space
 //n can only be put in the empty space if it follows rules of sudoku
 function check_if_number_can_go_in_position(table, n, r, c) {
-  // var below makes a array of tables chosen column that is need to search if n can be placed in empty position
-  var column_c = table.map((d) => d[c]);
+  // let below makes a array of tables chosen column that is need to search if n can be placed in empty position
+  let column_c = table.map((d) => d[c]);
   //case if empty position's row has number n already then the it need to re-evaluate the previous situation
   if (table[r].indexOf(n) != -1) {
     return false;
@@ -296,14 +296,14 @@ function check_if_number_can_go_in_position(table, n, r, c) {
 function sudoku_solver(table) {
   //r = row, c = column
   // function goes from right to left of table finding every empty space, empty
-  var values = find_empty_space(table);
+  let values = find_empty_space(table);
   // if values is false then the sudoku table is solved
   // if values is true then the sudoku table is not solved
   if (values === true) return true;
-  var r = values[0];
-  var c = values[1];
+  let r = values[0];
+  let c = values[1];
   // for loop, is so 1 to 4 sudoku numbers get run into empty position
-  for (var n = 1; n < 5; n++) {
+  for (let n = 1; n < 5; n++) {
     if (check_if_number_can_go_in_position(table, n, r, c) == true) {
       table[r][c] = n;
       if (sudoku_solver(table) === true) return true;
@@ -316,6 +316,6 @@ function sudoku_solver(table) {
 }
 
 function start() {
-  var table = sudoku_table;
+  let table = sudoku_table;
   sudoku_solver(table);
 }
